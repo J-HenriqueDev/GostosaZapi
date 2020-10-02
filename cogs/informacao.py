9ef08397d9140087745796ef55dbc4274f0a1990
Neo_ <45069999+neooprogrammer@users.noreply.github.com>
2020-10-02 09:23:31 -0300

    up

--- cogs/informacao.py
+++ cogs/informacao.py
@@ -44,7 +44,7 @@ class informacao(commands.Cog):
 
       embed = discord.Embed(title=texto, color=self.bot.cor)
       embed.set_image(url=usuario)
-      embed.set_footer(text=self.bot.user.name+" © 2019", icon_url=self.bot.user.avatar_url_as())
+      embed.set_footer(text=self.bot.user.name+" © 2020", icon_url=self.bot.user.avatar_url_as())
       await ctx.send(embed=embed)
   
     @commands.bot_has_permissions(embed_links=True)
@@ -92,6 +92,7 @@ class informacao(commands.Cog):
         geral = len([y.id for y in servidor.members])
         bots= len([y.id for y in servidor.members if y.bot])
         criado_em = str(servidor.created_at.strftime("%H:%M:%S - %d/%m/20%y"))
+        dias = (datetime.utcnow() - servidor.created_at).days
         usuarios = " <:nsonline:761304111152758846> : ``"+str(online)+"``<:nsocupado:761304111017754635> : ``"+str(afk)+"``  <:nsdnd:761304111336783872> : ``"+str(dnd)+"`` <:nsoffline:761304111361556520> : ``"+str(offline)+f"`` {self.bot._emojis['bots']} : ``"+str(bots)+"``"
         texto = f"{self.bot._emojis['texto']} : ``"+str(len(servidor.text_channels))+f"``{self.bot._emojis['voz']}  : ``"+str(len(servidor.voice_channels))+"``"
         cargos = len([y.id for y in servidor.roles])
@@ -101,7 +102,7 @@ class informacao(commands.Cog):
         embed.add_field(name=f"{self.bot._emojis['dono']} Dono", value = "``"+str(servidor.owner)+"``")
         embed.add_field(name=f"{self.bot._emojis['nome']} Nome", value = "``"+str(servidor.name)+"``")
         embed.add_field(name=f"{self.bot._emojis['ip']} Id", value = "``"+str(servidor.id)+"``")
-        embed.add_field(name=f"{self.bot._emojis['notas']} Criação", value = "``"+str(criado_em)+"``")
+        embed.add_field(name=f"{self.bot._emojis['notas']} Criação", value =f"``{criado_em}`` ({dias} dias)")
         embed.add_field(name=f"{self.bot._emojis['roles']} Cargos", value = "``"+str(cargos)+"``")
         embed.add_field(name=f"{self.bot._emojis['emoji']} Emojis", value = "``"+str(emojis)+"``")
         embed.add_field(name=f"{self.bot._emojis['canais']} Canais", value = texto)
@@ -109,65 +110,64 @@ class informacao(commands.Cog):
         embed.add_field(name=f"{self.bot._emojis['cadeado']} Verificação", value = "``"+str(servidor.verification_level).replace("none","Nenhuma").replace("low","Baixa").replace("medium","Média").replace("high","Alta").replace("extreme","Muito alta")+"``")
         embed.add_field(name=f" Usuários"+" ["+str(geral)+"]", value = usuarios)
         embed.set_thumbnail(url=img)
-        embed.set_footer(text=self.bot.user.name+" © 2019", icon_url=self.bot.user.avatar_url_as())
+        embed.set_footer(text=self.bot.user.name+" © 2020", icon_url=self.bot.user.avatar_url_as())
         await ctx.send(embed = embed)
 
     @commands.bot_has_permissions(embed_links=True)
     @commands.guild_only()
     @commands.command(description='Mostra as informações de um usuário.',usage='c.userinfo @TOBIAS',aliases=['uinfo', 'usuario'])
     async def userinfo(self, ctx, *, user: discord.Member = None):
-      
-      if user == None:
+      if user is None:
         usuario = ctx.author
         titulo = "Olá {}, esse é o seu perfil e aqui estão suas informações.".format(ctx.author.name)
       else:
         usuario = user
         titulo = "Olá {}, este é o perfil de {} e nele contém umas informações.".format(ctx.author.name, usuario.name)
 
-        if usuario.display_name == usuario.name:
-            apelido = "Não defindo"
-        else:
-          apelido = usuario.display_name
-        if usuario.avatar_url_as()  == "":
-          img = "https://i.imgur.com/To9mDVT.png"
-        else:
-          img = usuario.avatar_url_as()
-        try:
-          jogo = usuario.activity.name
-        except:
-            jogo = "No momento nada."
-        if usuario.id in [y.id for y in ctx.guild.members if not y.bot]:
-          bot = "Não"
-        else:
-          bot = "Sim"
-        svs = ', '.join([c.name for c in self.bot.guilds if usuario in c.members])
-        entrou_servidor = str(usuario.joined_at.strftime("%d/%m/20%y ás %H:%M:%S"))
-        conta_criada = str(usuario.created_at.strftime("%d/%m/20%y"))
-        conta_dias = (datetime.utcnow() - usuario.created_at).days
-        cargos = len([r.name for r in usuario.roles if r.name != "@everyone"])
-        if not svs: 
-          svs = 'Nenhum servidor em comum.'
-        on = "Disponível"
-        off = "Offline"
-        dnd = "Não Pertubar"
-        afk = "Ausente"
-        stat = str(usuario.status).replace("online",on).replace("offline",off).replace("dnd",dnd).replace("idle",afk)
-        cargos2 = len([y.id for y in ctx.guild.roles])
-        embed = discord.Embed(description=titulo,colour=self.bot.cor)
-        embed.set_author(name=f"Informação de perfil", icon_url=ctx.author.avatar_url_as())
-        embed.add_field(name=f"{self.bot._emojis['tag']} Tag", value = "``"+str(usuario.name)+"#"+str(usuario.discriminator)+"``")
-        embed.add_field(name=f"{self.bot._emojis['ip']} Id", value = "``"+str(usuario.id)+"``")
-        embed.add_field(name=f"{self.bot._emojis['nome']} Apelido", value = "``"+str(apelido)+"``")
-        embed.add_field(name=f"{self.bot._emojis['notas']} Criação da conta", value =f"``{conta_criada}`` ({conta_dias} dias)")
-        embed.add_field(name=f"{self.bot._emojis['entrou']} Entrou aqui em", value = "``"+str(entrou_servidor)+"``")
-        embed.add_field(name=f"{self.bot._emojis['toprole']} Maior cargo", value = "``"+str(usuario.top_role)+"``")
-        embed.add_field(name=f"{self.bot._emojis['roles']} Cargos", value = "``"+str(cargos)+"/"+str(cargos2)+"``")
-        embed.add_field(name=f"{self.bot._emojis['bots']} Bot", value = "``"+str(bot)+"``")
-        embed.add_field(name=f"{self.bot._emojis['status']} Status", value = "``"+str(stat)+"``")
-        embed.add_field(name=f"{self.bot._emojis['discord']} Servidores em comun",value=f"`{svs}`")
-        embed.set_thumbnail(url=img)
-        embed.set_footer(text=self.bot.user.name+" © 2019", icon_url=self.bot.user.avatar_url_as())
-        await ctx.send(embed = embed)
+      if usuario.display_name == usuario.name:
+          apelido = "Não defindo"
+      else:
+        apelido = usuario.display_name
+      if usuario.avatar_url_as()  == "":
+        img = "https://i.imgur.com/To9mDVT.png"
+      else:
+        img = usuario.avatar_url_as()
+      try:
+        jogo = usuario.activity.name
+      except:
+          jogo = "No momento nada."
+      if usuario.id in [y.id for y in ctx.guild.members if not y.bot]:
+        bot = "Não"
+      else:
+        bot = "Sim"
+      svs = ', '.join([c.name for c in self.bot.guilds if usuario in c.members])
+      entrou_servidor = str(usuario.joined_at.strftime("%d/%m/20%y ás %H:%M:%S"))
+      conta_criada = str(usuario.created_at.strftime("%d/%m/20%y"))
+      conta_dias = (datetime.utcnow() - usuario.created_at).days
+      cargos = len([r.name for r in usuario.roles if r.name != "@everyone"])
+      if not svs: 
+        svs = 'Nenhum servidor em comum.'
+      on = "Disponível"
+      off = "Offline"
+      dnd = "Não Pertubar"
+      afk = "Ausente"
+      stat = str(usuario.status).replace("online",on).replace("offline",off).replace("dnd",dnd).replace("idle",afk)
+      cargos2 = len([y.id for y in ctx.guild.roles])
+      embed = discord.Embed(description=titulo,colour=self.bot.cor)
+      embed.set_author(name=f"Informação de perfil", icon_url=ctx.author.avatar_url_as())
+      embed.add_field(name=f"{self.bot._emojis['tag']} Tag", value = "``"+str(usuario.name)+"#"+str(usuario.discriminator)+"``")
+      embed.add_field(name=f"{self.bot._emojis['ip']} Id", value = "``"+str(usuario.id)+"``")
+      embed.add_field(name=f"{self.bot._emojis['nome']} Apelido", value = "``"+str(apelido)+"``")
+      embed.add_field(name=f"{self.bot._emojis['notas']} Criação da conta", value =f"``{conta_criada}`` ({conta_dias} dias)")
+      embed.add_field(name=f"{self.bot._emojis['entrou']} Entrou aqui em", value = "``"+str(entrou_servidor)+"``")
+      embed.add_field(name=f"{self.bot._emojis['toprole']} Maior cargo", value = "``"+str(usuario.top_role)+"``")
+      embed.add_field(name=f"{self.bot._emojis['roles']} Cargos", value = "``"+str(cargos)+"/"+str(cargos2)+"``")
+      embed.add_field(name=f"{self.bot._emojis['bots']} Bot", value = "``"+str(bot)+"``")
+      embed.add_field(name=f"{self.bot._emojis['status']} Status", value = "``"+str(stat)+"``")
+      embed.add_field(name=f"{self.bot._emojis['discord']} Servidores em comun",value=f"`{svs}`")
+      embed.set_thumbnail(url=img)
+      embed.set_footer(text=self.bot.user.name+" © 2020", icon_url=self.bot.user.avatar_url_as())
+      await ctx.send(embed = embed)
 
 
     @commands.guild_only()
@@ -233,7 +233,7 @@ class informacao(commands.Cog):
           pass         
 
 
-        embed.set_footer(text=self.bot.user.name+" © 2019", icon_url=self.bot.user.avatar_url_as())
+        embed.set_footer(text=self.bot.user.name+" © 2020", icon_url=self.bot.user.avatar_url_as())
         await ctx.send(embed = embed)
 
     
@@ -263,7 +263,7 @@ class informacao(commands.Cog):
         perm = f"{perms_check(role.permissions)}"
         embed.add_field(name=f"{self.bot._emojis['cadeado']} Permissões:", value=f"``{perm.replace('use_voice_activation','Usar detecção de voz').replace('add_reactions','Adicionar reações').replace('administrator','Administrador').replace('attach_files','Anexar arquivos').replace('ban_members','Banir membros').replace('change_nickname','Mudar apelido').replace('connect','Conectar').replace('create_instant_invite','Criar um convite instatâneo').replace('deafen_members','Desativar áudio de membros').replace('embed_links','Inserir Links').replace('external_emojis','Emojis externos').replace('kick_members','Expulsar membros').replace('manage_channels','Gerenciar canais').replace('manage_emojis','Gerenciar emojis').replace('manage_guild','Gerenciar o servidor').replace('manage_messages','Gerenciar Mensagens').replace('manage_nicknames','Gerenciar apelidos').replace('manage_roles','Gerenciar cargos').replace('manage_webhooks','Gerenciar Webhooks').replace('mention_everyone','Mencionar todos').replace('move_members','Mover membros').replace('mute_members','Silenciar membros').replace('read_message_history','Ler histórico de mensagens').replace('read_messages','Ler mensagens').replace('send_messages','Enviar mensagens').replace('send_tts_messages','Enviar mensagem TTS').replace('speak','Falar').replace('view_audit_log','Ver registro de auditoria')}``")
         embed.set_thumbnail(url='https://htmlcolors.com/color-image/{}.png'.format(str(role.color).strip("#")))
-        embed.set_footer(text=self.bot.user.name+" © 2019", icon_url=self.bot.user.avatar_url_as())
+        embed.set_footer(text=self.bot.user.name+" © 2020", icon_url=self.bot.user.avatar_url_as())
         await ctx.send(embed=embed)
 
 
@@ -289,7 +289,7 @@ class informacao(commands.Cog):
         embed.add_field(name=" Link:", value=f"[``Clique aqui``]({emoji.url})", inline=False)
         embed.add_field(name=" Adicionado em:", value=f"``{emoji.created_at.__format__('%d/%m/%Y às %H:%M')}``", inline=False)
         embed.set_thumbnail(url=emoji.url)
-        embed.set_footer(text=f'{self.bot.user.name}© 2019', icon_url="https://i.imgur.com/Me7NqbZ.jpg") 
+        embed.set_footer(text=f'{self.bot.user.name}© 2020', icon_url="https://i.imgur.com/Me7NqbZ.jpg") 
         await ctx.send(embed=embed)
 
     
