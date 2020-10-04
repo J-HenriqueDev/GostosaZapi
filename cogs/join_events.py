4891b143e25946627d0ffd5e943762221d6be156
Neo_ <45069999+neooprogrammer@users.noreply.github.com>
2020-10-04 11:20:16 -0300

    Update join_events.py

--- cogs/join_events.py
+++ cogs/join_events.py
@@ -19,13 +19,14 @@ class bemvindo(commands.Cog):
     @commands.Cog.listener()  
     async def on_member_join(self, member):
        if member.guild.id == 758823253825028167:
-        canal = member.guild.get_channel(759814499661774869)
+        dev = member.guild.get_role(759814435031875586)
+        await member.add_roles(dev)
+        canal = self.bot.get_channel(759814499661774869)
         membros = len(member.guild.members)
         texto = "<a:emoji:760195465727180852> | **Membros** : "+str(membros).replace("0", "0⃣").replace("1", "1⃣").replace("2", "2⃣").replace("3", "3⃣").replace("4", "4⃣").replace("5", "5⃣").replace("6", "6⃣").replace("7", "7⃣").replace("8", "8⃣").replace("9", "9⃣")
         txt = f"{member} entrou no servidor."
         await canal.edit(topic=texto, reason=txt)
-        dev = member.guild.get_role(759814435031875586)
-        await member.add_roles(dev)
+        
         #########################################
 
 
@@ -75,7 +76,7 @@ class bemvindo(commands.Cog):
     @commands.Cog.listener()  
     async def on_member_remove(self, member):
        if member.guild.id == 758823253825028167:
-        canal = member.guild.get_channel(759814499661774869)
+        canal = self.bot.get_channel(759814499661774869)
         membros = len(member.guild.members)
         texto = "<a:emoji:760195465727180852> | **Membros** : "+str(membros).replace("0", "0⃣").replace("1", "1⃣").replace("2", "2⃣").replace("3", "3⃣").replace("4", "4⃣").replace("5", "5⃣").replace("6", "6⃣").replace("7", "7⃣").replace("8", "8⃣").replace("9", "9⃣")
         txt = f"{member} saiu do servidor."
